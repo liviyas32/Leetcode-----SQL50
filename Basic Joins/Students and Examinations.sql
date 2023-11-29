f4e6141c926a9c1c89cfad597ae1440324db0cfd
liviyas32 <125739869+liviyas32@users.noreply.github.com>
2023-11-29 17:36:44 +0530

    Update Students and Examinations.sql

--- Basic Joins/Students and Examinations.sql
+++ Basic Joins/Students and Examinations.sql
@@ -1,5 +1,5 @@
 select st.student_id, st.student_name, su.subject_name, count(e.subject_name) as attended_exams
-from students as st join subjects as su left join examinations as e on st.student_id = e.student_id
-and su.subject_name = e.subject_name
+from students as st join subjects as su left join examinations as e 
+on st.student_id = e.student_id and su.subject_name = e.subject_name
 group by st.student_id, su.subject_name
 order by st.student_id, su.subject_name;
