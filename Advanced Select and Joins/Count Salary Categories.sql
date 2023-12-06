4b3a7f052319c4351bc253afddd7a43c220184d6
liviyas32 <125739869+liviyas32@users.noreply.github.com>
2023-12-06 17:37:20 +0530

    Create Count Salary Categories.sql

---
+++ Advanced Select and Joins/Count Salary Categories.sql
@@ -0,0 +1,11 @@
+(select 'Low Salary' as category, count(account_id) as accounts_count
+from accounts
+where income<20000)
+union
+(select 'Average Salary' as category, count(account_id) as accounts_count
+from accounts
+where income between 20000 and 50000)
+union
+(select 'High Salary' as category, count(account_id) as accounts_count
+from accounts
+where income>50000);
